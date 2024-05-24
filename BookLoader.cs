4ba1c553bcce0eaa0e4e970870600e4ce42f13fa
Itorius <itorius.yt@gmail.com>
2024-05-24 10:49:26 +0200

    Cleanup

---
+++ BookLoader.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace BookLibrary;
+
+public static class BookLoader
+{
+	internal static readonly IList<ModBook> items = new List<ModBook>();
+	public static int ItemCount { get; private set; }
+
+	internal static int Register(ModBook item)
+	{
+		items.Add(item);
+		return ItemCount++;
+	}
+}
+
+public abstract class ModBook : ModType, ILocalizedModType
+{
+	public string LocalizationCategory => "Books";
+
+	public readonly List<BookCategory> Categories = [];
+
+	public virtual string Texture => BaseLibrary.BaseLibrary.PlaceholderTexture;
+	public virtual LocalizedText DisplayName => this.GetLocalization("Name", PrettyPrintName);
+
+	public void AddCategory(BookCategory category)
+	{
+		category.Mod = this;
+		Categories.Add(category);
+	}
+
+	protected sealed override void Register()
+	{
+		ModTypeLookup<ModBook>.Register(this);
+		BookLoader.Register(this);
+	}
+
+	public sealed override void SetupContent()
+	{
+		SetStaticDefaults();
+	}
+}
