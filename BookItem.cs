b23d90194f1bb4bf4220e1340c23beb961a472fa
itorius <itorius.yt@gmail.com>
2025-01-10 12:09:04 +0100

    Fix translations not being loaded when opening book

--- BookItem.cs
+++ BookItem.cs
@@ -11,7 +11,7 @@ public class BookItem : BaseItem
 	{
 		Item.useTime = 30;
 		Item.useAnimation = 30;
-		Item.useStyle = ItemUseStyleID.Guitar;
+		Item.useStyle = ItemUseStyleID.RaiseLamp;
 		Item.rare = ItemRarityID.White;
 	}
 
@@ -20,7 +20,10 @@ public class BookItem : BaseItem
 	public override bool? UseItem(Player player)
 	{
 		if (Main.netMode != NetmodeID.Server && player.whoAmI == Main.LocalPlayer.whoAmI)
+		{
 			BookUI.Instance.Display = BookUI.Instance.Display == Display.Visible ? Display.None : Display.Visible;
+			BookUI.Instance.Recalculate();
+		}
 
 		return true;
 	}
